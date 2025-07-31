3f3f3f07bf75439eb2588faea99de191fece22f0
Narava Rupa Sri <n.rupasri25@gmail.com>
2025-07-31 10:39:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+import time_series_visualizer
+
+# Run all visualizations
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
